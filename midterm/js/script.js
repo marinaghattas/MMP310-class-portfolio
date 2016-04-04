46987233d8be94386b47a89ce4f1113f882f573c
marinaghattas <marina.ghattas@f1006-10-34127.bmcc.cuny.edu>
2016-04-04 18:38:14 -0400

    midterm file done

--- midterm/js/script.js
+++ midterm/js/script.js
@@ -1,14 +1,16 @@
 var submit = document.getElementById("submit");
-submit.onclick = function() {
+submit.onclick = function () {
 
-  var name = document.getElementById("name").value;
-  var old = document.getElementById("old").value;
-  var sport = document.getElementById("sport").value;
-  var color = document.getElementById("color").value;
-  var food = document.getElementById("food").value;
-  var place = document.getElementById("place").value;
+    document.querySelector("#form").style.display = "none";
+
+    var name = document.getElementById("name").value;
+    var old = document.getElementById("old").value;
+    var sport = document.getElementById("sport").value;
+    var color = document.getElementById("color").value;
+    var food = document.getElementById("food").value;
+    var place = document.getElementById("place").value;
 
     var story = document.getElementById("story");
-  story.innerHTML = "My name is " +
-  name + " and I am" + old + "My favorite sport is " + sport + " My favorite color is " + color +  ". i love " + food + " and i really want to go visit " + place + " Thank you" +  "." ;
+    story.innerHTML = "My name is " +
+        name + " and I am " + old + "years old. My favorite sport is " + sport + " My favorite color is " + color + ". i love " + food + " and i really want to go visit " + place + ". " + "<br> <br> Thank you " + "☺.";
 }
