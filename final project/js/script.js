a8efa3eaac118ed4de45c4e53da9ccd8891c699d
marinaghattas <marina.ghattas@f1006-10-34127.bmcc.cuny.edu>
2016-04-20 18:35:08 -0400

    added final project file

---
+++ final project/js/script.js
@@ -0,0 +1,126 @@
+var currentLevel;
+var pacman;
+var collectibles;
+
+
+
+
+function setup() {
+   // console.log('setup');
+    createCanvas(1195, 725);
+
+    pacman = createSprite(1150,50,0,0);
+    pacman.addAnimation("still", "images/pacman1.png","images/pacman2.png");
+
+    //create two groups
+    obstacles = new Group();
+    collectibles = new Group();
+
+    setupCanvas()
+    currentLevel = 0; //start at level one
+    var url = "data/setup.json";
+    loadJSON(url, jsonLoaded);
+
+}
+
+function setupCanvas(){
+    background('#222222');
+}
+
+
+
+function draw(){
+    setupCanvas();
+    fill('green')
+    ;
+    drawSprites();
+
+    pacman.collide(obstacles)
+    pacman.overlap(collectibles, collect)
+
+}
+
+function jsonLoaded(data) {
+    console.log('jsonLoaded',data);
+    var walls = data.levels[currentLevel].walls;
+    console.log('walls',walls);
+    for (var i = 0; i < walls.length; i++) {
+        var wall = createSprite(walls[i][0], walls[i][1], walls[i][2], walls[i][3]);
+        obstacles.add(wall);
+
+        wall.wallx = walls[i][2];
+        wall.wally = walls[i][3];
+        wall.draw = function(){
+            fill(data.levels[currentLevel].wallColor);
+            rect(0,0, this.wallx, this.wally );
+        }
+
+    }
+
+
+    var dots = data.levels[currentLevel].dots;
+    console.log('dots',dots);
+    for (var i = 0; i < dots.length; i++) {
+        var dot = createSprite(dots[i][0], dots[i][1], dots[i][2], dots[i][3]);
+        collectibles.add(dot);
+
+         dot.draw = function(){
+            fill(data.levels[currentLevel].dotsColor);
+            ellipse(0,0, 10,10);
+        }
+    }
+
+
+}
+
+
+
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+//    circle.position.y -= 5;
+      pacman.setSpeed(2,270);
+      pacman.changeAnimation("still");
+  } else if (keyCode == DOWN_ARROW) {
+    pacman.position.y += 5;
+      pacman.setSpeed(2,90);
+      pacman.changeAnimation("down");
+  }
+    if (keyCode == LEFT_ARROW) {
+//    circle.position.y -= 5;
+      pacman.setSpeed(2,180);
+      pacman.changeAnimation("still");
+  } else if (keyCode == RIGHT_ARROW) {
+    pacman.position.x -= 5;
+      pacman.setSpeed(2,360);
+      pacman.changeAnimation("Left");
+      //return false; // prevent default
+  }
+}
+
+function collect(collector, collected)
+{
+
+  collected.remove();
+}
+
+var seconds = 60;
+
+function secondPassed() {
+  var minutes = Math.round((seconds - 30)/60),
+      remainingSeconds = seconds % 60;
+  
+  if (remainingSeconds < 10) {
+    remainingSeconds = "0" + remainingSeconds;  
+  }
+  
+  document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
+  if (seconds == 0 && dots == 0 ) {
+    clearInterval(countdownTimer);
+    document.getElementById('countdown').innerHTML = "YOU LOST";
+  } else {
+    seconds--;
+  }
+}
+ 
+var countdownTimer = setInterval('secondPassed()', 1000);
+
